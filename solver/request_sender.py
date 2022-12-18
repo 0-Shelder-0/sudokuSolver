487f8023d278ec3ce76cbe1916f7fcff880ecc10
Vladislav Piligrimov <vladpiligrimov@gmail.com>
2022-12-19 01:35:24 +0500

    fixed sending requests in solver

--- solver/request_sender.py
+++ solver/request_sender.py
@@ -13,17 +13,17 @@ BASE_URL = environ.get("WEB_API_BASE_URL")
 
 def send_request_to_create_status(solution_id: int):
     model = SolutionStatusCreate(solution_id, Status.IN_PROGRESS)
-    send_request(f'{BASE_URL}/status/', model)
+    send_request('post', f'{BASE_URL}/status/', model)
 
 
 def send_request_to_update_solution(solution, solution_id):
     model = SolutionUpdate(solution_id, solution)
-    send_request(f'{BASE_URL}/solution/', model)
+    send_request('put', f'{BASE_URL}/solution/{solution_id}', model)
 
 
-def send_request(web_url, model):
+def send_request(method, web_url, model):
     web_request_body = json.dumps(model)
     headers = {'api-token': API_TOKEN}
 
-    web_response = requests.put(web_url, json=web_request_body, headers=headers, timeout=10)
+    web_response = requests.request(method=method, url=web_url, json=web_request_body, headers=headers, timeout=10)
     web_response.raise_for_status()
