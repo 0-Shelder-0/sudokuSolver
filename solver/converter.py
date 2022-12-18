59c4c3fcb15abdb52ea12bdc4bebdc6c8ca1b010
Vladislav Piligrimov <vladpiligrimov@gmail.com>
2022-12-19 01:00:40 +0500

    fixed solver

--- solver/converter.py
+++
@@ -1,8 +0,0 @@
-def convert_to_matrix(solution):
-    # todo реализовать конвертацию
-    return solution
-
-
-def convert_to_str(solution):
-    # todo реализовать конвертацию
-    return solution
--- solver/sudoku_solver.py
+++ solver/sudoku_solver.py
@@ -1,14 +1,3 @@
-from solver.converter import convert_to_matrix, convert_to_str
-
-
-def get_solution(initial_solution: str) -> str:
-    matrix = convert_to_matrix(initial_solution)
-    solution = solve(matrix)
-    result = convert_to_str(solution)
-
-    return result
-
-
-def solve(matrix):
+def get_solution(initial_solution):
     # todo реализовать механизм решения
-    return matrix
+    return initial_solution
